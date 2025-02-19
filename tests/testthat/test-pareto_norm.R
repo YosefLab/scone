39212ebf0244d771dfc8633fa2f7dac0e0a56775
Hervé Pagès <hpages.on.github@gmail.com>
2025-02-18 22:53:28 -0800

    Repair and optimize PsiNorm()
    A recent change in DelayedArray (in version 0.33.5) broke PsiNorm()
    on a sparse DelayedMatrix object and on a SE/SCE object with counts
    represented as a sparse DelayedMatrix object.
    This commit repairs that and introduces two small improvements to
    PsiNorm():
    - makes PsiNorm() work on a SparseMatrix object;
    - makes PsiNorm() slightly faster by reducing the number of matrix
      transpositions from 4 to 2.

--- tests/testthat/test-pareto_norm.R
+++ tests/testthat/test-pareto_norm.R
@@ -1,14 +1,33 @@
 test_that("PSiNorm works with all input classes", {
-  m <- matrix(c(1,0,2,0,2,9,3,0), ncol=2)
-  
+  m <- matrix(0L, nrow=4, ncol=5)
+  m[c(1,3) , 1] <- 1:2
+  m[1:3, 3] <- c(2L, 9L, 3L)
+  m[ , 4] <- 2:1
+  m[2, 5] <- 1L
+
   out1 <- PsiNorm(m)
-  
+
+  svt <- as(m, "SVT_SparseMatrix")
+  out_svt <- PsiNorm(svt)
+  expect_true(is(out_svt, "SVT_SparseMatrix"))
+  expect_equal(out1, as.matrix(out_svt))
+
+  dm1 <- DelayedArray::DelayedArray(m)
+  out_dm1 <- PsiNorm(dm1)
+  expect_true(is(out_dm1, "DelayedMatrix"))
+  expect_equal(out1, as.matrix(out_dm1))
+
+  dm2 <- DelayedArray::DelayedArray(svt)
+  out_dm2 <- PsiNorm(dm2)
+  expect_true(is(out_dm2, "DelayedMatrix"))
+  expect_equal(out1, as.matrix(out_dm2))
+
   se <- SummarizedExperiment(m)
   sce <- SingleCellExperiment(m)
-  
+
   out_se <- PsiNorm(se)
   expect_equal(out1, assay(out_se, "PsiNorm"))
-  
+
   out_sce <- PsiNorm(sce, whichAssay = 1)
   out2 <- t(t(m)/sizeFactors(out_sce))
   expect_equal(out1, out2)
